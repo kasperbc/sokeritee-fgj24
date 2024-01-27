dcb6f8af2c5e7d75a5991ba3dc63feee15966a6f
Kasper Björkgren <kasper.bjorkgren@gmail.com>
2024-01-27 14:14:49 +0200

    fixed

--- Assets/PreviousWork/KevinScripts/LegController.cs
+++ Assets/PreviousWork/KevinScripts/LegController.cs
@@ -6,6 +6,7 @@ public class LegController : MonoBehaviour
 {
     public Transform leg;
     public KeyCode moveKey;
+    public KeyCode moveSideKey;
     [SerializeField] private float rotationSpeed = 60f;
     [SerializeField] private float maxForwardAngle = 90f;
     [SerializeField] private float maxBackwardAngle = -40f;
@@ -14,23 +15,15 @@ public class LegController : MonoBehaviour
     private bool isRotating = false;
     private int rotationDirection = -1;
 
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetKey(moveKey))
         {
-            isRotating = true;
-        }
-        else
-        {
-            isRotating = false;
-        }
-    }
-
-    void FixedUpdate()
-    {
-        if (isRotating)
-        {
             RotateLeg();
+        }
+        if (Input.GetKey(moveSideKey))
+        {
+            RotateLegSide();
         }
     }
 
@@ -60,6 +53,35 @@ public class LegController : MonoBehaviour
         newAngle = Mathf.Clamp(newAngle, maxBackwardAngle, maxForwardAngle);
 
 
-        leg.localRotation = Quaternion.Euler(0f, 0f, newAngle);
+        leg.localRotation = Quaternion.Euler(leg.localEulerAngles.x, leg.localEulerAngles.y, newAngle);
+    }
+
+    void RotateLegSide()
+    {
+        float rotationAmount = rotationSpeed * Time.fixedDeltaTime;
+
+
+        float currentAngle = leg.localRotation.eulerAngles.x;
+
+
+        float newAngle = currentAngle + rotationAmount * rotationDirection;
+
+
+        if (newAngle > 180f)
+        {
+            newAngle -= 360f;
+        }
+
+
+        if (newAngle >= maxForwardAngle || newAngle <= maxBackwardAngle)
+        {
+            rotationDirection *= -1; // Change direction
+        }
+
+
+        newAngle = Mathf.Clamp(newAngle, maxBackwardAngle, maxForwardAngle);
+
+        
+        leg.localRotation = Quaternion.Euler(newAngle, leg.localEulerAngles.y, leg.localEulerAngles.z);
     }
 }

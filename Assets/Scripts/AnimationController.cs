b95ad8fe8eb77cf53e98e98b27caab4a6a9f4199
kelethe0809 <kelethe@outlook.com>
2024-01-27 17:09:30 +0200

    push Aki's scene and script to main
    push Aki's scene and script to main

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    private Animation _animation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animation = GetComponent<Animation>();
+    }
+
+    public void PlayAnimation()
+    {
+        _animation.Play("CameraAnimation");
+    }
+}
